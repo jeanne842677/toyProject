d7d241ce6d6b138086ebea131dda4025711ca5d2
jeanne8426 <jeanne842677@gmail.com>
2022-04-13 20:50:22 +0900

    commit

---
+++ toyProject/src/test/java/com/kh/spring/admin/AdminControllerTest.java
@@ -0,0 +1,44 @@
+package com.kh.spring.admin;
+
+
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebAppConfiguration
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/**/*-context.xml"})
+public class AdminControllerTest {
+
+	@Autowired
+	WebApplicationContext wac;
+	MockMvc mockMvc;
+	
+	@Before
+	public void setup() {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+	}
+	
+	@Test
+	public void searchAllMembers() throws Exception {
+		mockMvc.perform(get("/admin/member/member-list"))
+		.andExpect(status().isOk())
+		.andDo(print());
+	}
+
+	
+}
